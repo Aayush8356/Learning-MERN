0605e735c39c874b925c7c68d3e62d040cb77800
Aayush Gupta <guptaaayush537@gmail.com>
2022-05-01 14:40:38 +0530

    First Look

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The web App</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">category</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
