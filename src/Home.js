9b7a8d4e67c5f9c00d402938ddc5ce6deed448a5
Aayush Gupta <guptaaayush537@gmail.com>
2022-05-01 22:19:51 +0530

    modified

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 const Home = () => {
+    let list = ['Apoorv','Mayank','Yash','Ashutosh'];
+    const [name,setName] = useState('Aayush');
+    const [age,setAge] = useState(10);
+    const clickHandle = () =>
+    {
+        setName(list[Math.floor(Math.random()*4)]);
+        setAge(Math.floor(Math.random()*10));
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p> {name} {age} </p>
+            <button onClick={clickHandle}>click me</button>
         </div>
+        
      );
+     
 }
  
 export default Home;
